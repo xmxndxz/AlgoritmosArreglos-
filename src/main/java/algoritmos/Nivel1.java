a4ca96db88754b513eae6ea39350c43f6555c098
amand <amand@yaya.tigo.co.cr>
2026-09-12 17:10:43 -0600

    se comienza el nivel 1 Creacion de mi primer y segundo algortimo en el nivel 1

--- src/main/java/algoritmos/Nivel1.java
+++ src/main/java/algoritmos/Nivel1.java
@@ -4,12 +4,43 @@
  */
 package algoritmos;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author amand
+ * @author amand //AmandaAR
  */
 public class Nivel1 {
-    public void suma(){
-        
+
+    //Algoritmo 3
+    public static void metodo3() {
+        int[] numeros = new int[8]; 
+
+        for (int i = 0; i < numeros.length; i++) {
+            String entrada = JOptionPane.showInputDialog("Ingrese el número entero #" + (i + 1) + " de 8:");
+            numeros[i] = Integer.parseInt(entrada);
+        }
+
+        // Recorrido para mostrar los valores, uno por línea
+        String resultado = "Valores ingresados:\n";
+        for (int i = 0; i < numeros.length; i++) {
+            resultado += numeros[i] + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+
+    //Algortimo 4
+    public static void metodo4() {
+        String palabra = JOptionPane.showInputDialog("Ingrese una palabra conocida:");
+        char[] letras = palabra.toCharArray(); // arreglo unidimensional de tipo primitivo char
+
+        String resultado = "Caracteres de la palabra:\n";
+        for (int i = 0; i < letras.length; i++) {
+            resultado += letras[i] + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
     }
 }
+
