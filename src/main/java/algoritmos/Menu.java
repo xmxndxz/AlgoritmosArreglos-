d456009f3fff2cfd421ab89e7d1b00f8cec646d8
amand <amand@yaya.tigo.co.cr>
2026-09-12 17:39:10 -0600

    Creacion del Menu

--- src/main/java/algoritmos/Menu.java
+++ src/main/java/algoritmos/Menu.java
@@ -4,10 +4,78 @@
  */
 package algoritmos;
 
-/**
- *
- * @author amand
- */
+import javax.swing.JOptionPane;
+
+//@author amand
 public class Menu {
+
+    public static void iniciar() {
+        int opcion;
+        do {
+            String entrada = JOptionPane.showInputDialog("""
+                                                         MEN\u00da PRINCIPAL
+                                                         1. Nivel 1
+                                                         2. Nivel 2
+                                                         3. Nivel 3
+                                                         4. Nivel 4
+                                                         5. Nivel 5
+                                                         0. Salir
+                                                         Seleccione una opci\u00f3n:""");
+            opcion = Integer.parseInt(entrada);
+
+            switch (opcion) {
+                case 1:
+                    menuNivel1();
+                    break;
+                case 2:
+                    //menuNivel2();
+                    break;
+                case 3:
+                    //menuNivel3();
+                    break;
+                case 4:
+                   // menuNivel4();
+                    break;
+                case 5:
+                    //menuNivel5();
+                    break;
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Saliendo del programa...");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción no válida.");
+            }
+        } while (opcion != 0);
+    }
+
     
+    
+    // Submenú Nivel 1
+    private static void menuNivel1() {
+        int opcion;
+        do {
+            String entrada = JOptionPane.showInputDialog(
+                    "NIVEL 1\n"
+                    + "3. Leer 8 números y mostrarlos\n"
+                    + "4. Palabra a arreglo de caracteres\n"
+                    + "0. Volver al menú principal\n"
+                    + "Seleccione una opción:"
+            );
+            opcion = Integer.parseInt(entrada);
+
+            switch (opcion) {
+                case 3:
+                    Nivel1.metodo3();
+                    break;
+                case 4:
+                    Nivel1.metodo4();
+                    break;
+                case 0:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción no válida.");
+            }
+        } while (opcion != 0);
+    }
+
 }
