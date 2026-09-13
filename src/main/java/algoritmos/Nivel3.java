74e14678345fa900b492b66f4bfbdaa5988ca302
amand <amand@yaya.tigo.co.cr>
2026-09-12 18:12:32 -0600

    Iniacilización del Nivel 3 empezando el segundo algoritmo

--- src/main/java/algoritmos/Nivel3.java
+++ src/main/java/algoritmos/Nivel3.java
@@ -4,19 +4,38 @@
  */
 package algoritmos;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author amand
  */
-public class Nivel3 {
+  
 
-    public static void metodo11() {
-        int[] numeros = new int[12];
+    public class Nivel3 {
 
-        for (int i = 0; i < numeros.length; i++) {
-            String entrada = JOptionPane.showInputDialog("Ingrese el número #" + (i + 1) + " de 12:");
-            numeros[i] = Integer.
+        public static void metodo11() {
+            int[] numeros = new int[12];
+
+            for (int i = 0; i < numeros.length; i++) {
+                String entrada = JOptionPane.showInputDialog("Ingrese el número #" + (i + 1) + " de 12:");
+                numeros[i] = Integer.parseInt(entrada);
+            }
+
+            String entradaBuscado = JOptionPane.showInputDialog("Ingrese el valor que desea buscar en el arreglo:");
+            int buscado = Integer.parseInt(entradaBuscado);
+
+            int contador = 0; // contador de apariciones
+            for (int i = 0; i < numeros.length; i++) {
+                if (numeros[i] == buscado) {
+                    contador++;
+                }
+            }
+
+            JOptionPane.showMessageDialog(null,
+                    "El valor " + buscado + " aparece " + contador + " vez(es) en el arreglo.");
         }
-
     }
-}
+
+    public static void metodo12() {
+    }
