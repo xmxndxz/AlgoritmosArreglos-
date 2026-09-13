c08db5451ae1a9c30213bf146d73169339c1cb61
amand <amand@yaya.tigo.co.cr>
2026-09-12 19:02:47 -0600

    Inicializacion nivel 4

--- src/main/java/algoritmos/Nivel4.java
+++ src/main/java/algoritmos/Nivel4.java
@@ -4,10 +4,26 @@
  */
 package algoritmos;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author amand
  */
 public class Nivel4 {
-    
-}
+
+    // Algoritmo 15
+    public static void metodo15() {
+
+        int tam = -1;
+        boolean tamValido = false;
+        while (!tamValido) {
+            String entradaTam = JOptionPane.showInputDialog("Ingrese el tamaño del arreglo (mayor a 0):");
+            tam = Integer.parseInt(entradaTam);
+            if (tam > 0) {
+                tamValido = true;
+            } else {
+                JOptionPane.showMessageDialog(null, "El tamaño debe ser mayor a 0. Intente nuevamente.");
+            }
+        }
+    }
